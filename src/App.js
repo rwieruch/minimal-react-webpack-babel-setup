fa38e47bedd1f3a40de10d39c87a5d498fa056f8
Robin Wieruch <wrobin@gmx.net>
2019-08-06 15:53:02 +0200

    include image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const App = ({ title }) =>
-  <div>{title}</div>;
+import MyImage from './assets/images/myimage.jpg';
+
+const App = ({ title }) => (
+  <div>
+    <span>{title}</span>
+    <img src={MyImage} alt="torchlight in the sky" />
+  </div>
+);
 
 export default App;
